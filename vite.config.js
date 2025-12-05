8b14ea750e524912f0d453b632740356c3871aad
Avnish kumar <avnish.kumar@corpseed.com>
2025-12-05 16:31:10 +0530

    some minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port:3000,
+    port: 3000,
+    allowedHosts: ["www.eprcomply.com", "http://www.eprcomply.com/"],
     proxy: {
       "/api": {
         target: "http://localhost:8000",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, "/api"), 
-      }
-    }
-  }
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
 });
-
